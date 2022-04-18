631b0161166166b245e8eb118a60e5f16ba375f8
Vitaliy Dadayan <dadayan@e-krit.ru>
2022-04-18 16:17:09 +0300

    create project, add packets

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+const App = () => {
+    return (
+        <div>
+            <Container/>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, perferendis.
+        </div>
+    );
+};
+
+export default App;
