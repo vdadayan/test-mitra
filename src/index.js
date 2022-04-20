6c11a0a062cd4600e794aa59429b0405974c6d4d
Vitaliy Dadayan <dadayan@e-krit.ru>
2022-04-20 14:26:36 +0300

    gh pages

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter,HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
